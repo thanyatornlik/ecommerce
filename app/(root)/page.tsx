5b5fc04b92835914efbec2b936d8b28bdecac46d
thanyatornlik <thanyatorn.lik@gmail.com>
2024-05-06 23:03:08 +0700

    adding modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,25 @@
 // import Image from "next/image";
-import { Button } from '@/components/ui/button';
-export default function Home() {
-    return (
-      <div className="p-4">
-        <Button size='default' variant='outline'>Click me</Button>
-      </div>
-    );
-  }
-  
+"use client"
+import { Modal } from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(()=>{
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return (
+    <div className="p-4">
+      Root Page
+    </div>
+  );
+}
+
+export default SetupPage;
+
